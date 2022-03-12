5cc0d8366e8281aa8664cb53e16d44bf6a4f3ce1
Vasil Karatashev <karatashev@gmail.com>
2022-03-13 00:43:29 +0100

    finished adding deleting and importance lvl features

--- main.js
+++ main.js
@@ -6,25 +6,31 @@ const addBtn = document.getElementById('new-task-submit');
 const alertMsg = document.querySelector('#alertMsg');
 
 
-
-
-
 addBtn.addEventListener('click', () => {
   const task = newTaskInput.value;
 
-  let importanceLevel = '';
-  if (document.getElementById('slightly-important').checked) {
-    importanceLevel = document.getElementById('slightly-important').value;
-    console.log(importanceLevel);
-  } 
-  else if (document.getElementById('important').checked) {
-    importanceLevel = document.getElementById('important').value;
-    console.log(importanceLevel);
+  
+  if (task === '') {
+    alertMsg.innerText = 'You must write the name of the task!';
+    alertMsg.style.visibility = 'visible';
+    return;
   }
-  else  {
-    importanceLevel = document.getElementById('very-important').value;
-  };
+  
+  // simplier way to select the input value
+  let importanceLevel = document.querySelector('input[name="important"]:checked').value;
+  console.log(importanceLevel);
+  // if (document.getElementById('slightly-important').checked) {
+  //   importanceLevel = document.getElementById('slightly-important').value;
+  // } 
+  // else if (document.getElementById('important').checked) {
+  //   importanceLevel = document.getElementById('important').value;
+  // }
+  // else  {
+  //   importanceLevel = document.getElementById('very-important').value;
+  // };
 
+
+  //select the task list and create Task
   const tasksList = document.getElementById('tasks');
   const taskItem = document.createElement('div');
   taskItem.classList.add('task');
@@ -33,24 +39,33 @@ addBtn.addEventListener('click', () => {
           <div class="task-content">
             <input type="text" class="task-text" value="${task}" readonly>
           </div>
-        
-          <div class="task-buttons">
-            <span class="btn-importance-lvl">${importanceLevel}</span>
-            <button class="btn-delete">Delete</button>
-          </div>
-        </div>
-      `
-      tasksList.appendChild(taskItem);
-      newTaskInput.value = ''; 
-      importanceLevel = '';
-    
+        `
+  //add task buttons to the Task
+  const taskButtons = document.createElement('div');
+  taskButtons.classList.add('task-buttons');
+  taskItem.appendChild(taskButtons);
 
+  //add importance span info to the taks buttons
+  const taskImportance = document.createElement('span');
+  taskImportance.classList.add('btn-importance-lvl');
+  taskImportance.innerText = importanceLevel;
+  taskButtons.appendChild(taskImportance);
 
-  const deleteBtn = document.querySelector('.btn-delete');
-      
+  //create a delete button to start listening clicks
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('btn-delete');
+  deleteBtn.innerText = 'Delete';
   deleteBtn.addEventListener('click', () =>  {
     tasksList.removeChild(taskItem);
   });
+
+  //add delete button to the task buttons
+  taskButtons.appendChild(deleteBtn);
+
+      newTaskInput.value = ''; 
+  
+  // add the whole Task to the Task List
+  tasksList.appendChild(taskItem);
 
 });
 
